812c5a9733d8ab622d3f651203d84456156f5151
Reiflin98 <92531744+Reiflin98@users.noreply.github.com>
2021-10-28 13:12:50 +0200

    Lidt nyt

---
+++ Main/Main/main.c
@@ -0,0 +1,23 @@
+/*
+ * Main.c
+ *
+ * Author : PRJ1-E3
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    int counter(int)=0;
+	initSystem()
+	// starter systemtet op
+	
+	
+	playRelex()
+	counter++;
+	
+
+	
+}
+
