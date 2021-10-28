fa09d65fe8137481b09662e9955ab7e870ee1adf
Reiflin98 <92531744+Reiflin98@users.noreply.github.com>
2021-10-28 13:04:04 +0200

    main();

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Main.c
+ *
+ * Created: 28-10-2021 12:20:51
+ * Author : thoma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
