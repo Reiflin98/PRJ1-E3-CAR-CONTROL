b4bd319e7b7ac71a4b655245057c1144bcf059f8
Reiflin98 <92531744+Reiflin98@users.noreply.github.com>
2021-10-28 13:25:52 +0200

    Rettelse

---
+++ Main/BIL/main.c
@@ -0,0 +1,18 @@
+/*
+ * BIL.c
+ *
+ * Created: 28-10-2021 13:22:51
+ * Author : thoma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
