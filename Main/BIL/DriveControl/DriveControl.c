1c202bffde27c198f163c45aceb167c99359bdec
NiklasUls <niklasuls@live.dk>
2021-11-29 10:52:52 +0100

    Drivecontrol

--- Main/BIL/DriveControl/DriveControl.c
+++ Main/BIL/DriveControl/DriveControl.c
@@ -37,47 +37,47 @@ void driveControl()
 		switch (counter)
 		{
 		case 0: //ingen brik endnu
-			accelerateForward(50); //Accelerer inden 1. brik
+			carDrive(50); //Accelerer inden 1. brik
 			break;
 			
 		case 1: //Brik 1
-			accelerateForward(35); //Nedsæt hastigheden lidt (Bakke snart)
+			carDrive(35); //Nedsæt hastigheden lidt (Bakke snart)
 			break;
 			
 		case 2: //Brik 2
-			accelerateForward(20); //Bakke start - hastighed ydeligere ned
+			carDrive(20); //Bakke start - hastighed ydeligere ned
 			break;
 			
 		case 3: //Brik 3
-			accelerateForward(10); //Hastighed ned inden bakken kører ned
+			carDrive(10); //Hastighed ned inden bakken kører ned
 			break;
 			
 		case 4: //Brik 4
-			accelerateForward(50); //HAstigheden op - bunden af bakken
+			carDrive(50); //HAstigheden op - bunden af bakken
 			break;
 			
 		case 5: //Brik 5
-			accelerateForward(40); //hastigheden lidt ned - gør klar til snart at stoppe
+			carDrive(40); //hastigheden lidt ned - gør klar til snart at stoppe
 			break;
 			
 		case 6: //Brik 6
-			accelerateBackwards(30); //Kør tilbage til refleksbrik 5
+			carDrive(30); //Kør tilbage til refleksbrik 5
 			break;
 			
 		case 7: //Brik 6
-			accelerateBackwards(40); //Refelksbrik 6 igen
+			carDrive(40); //Refelksbrik 6 igen
 			break;
 			
 		case 8: //Brik 5
-			accelerateForward(50); //Kør frem igen
+			carDrive(50); //Kør frem igen
 			break;
 			
 		case 9: //Brik 5
-			accelerateForward(50); //Kør frem igen
+			carDrive(50); //Kør frem igen
 			break;
 			
 		case 10: //Brik 6
-			accelerateForward(30); //Kør frem igen	
+			carDrive(30); //Kør frem igen	
 			break;
 			
 		}
