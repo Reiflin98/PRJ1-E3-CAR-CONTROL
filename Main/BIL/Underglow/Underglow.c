34ac371af51ff0ae7c4dda1cafea9a40463b7da2
mqz1000 <themqz@live.com>
2021-12-14 12:25:43 +0100

    Underglow
    :)

--- Main/BIL/Underglow/Underglow.c
+++ Main/BIL/Underglow/Underglow.c
@@ -6,3 +6,12 @@
 
 #include <avr/io.h>
 #include "Underglow.h"
+
+void turnOnUnderglow (void)
+{
+	PORTC = PORTC | 0b00000100; // Sender 5V ud til C2
+}
+void turnOffUnderglow (void)
+{
+	PORTC = PORTC & 0b11111011; // Sender 0V ud til C2
+}
